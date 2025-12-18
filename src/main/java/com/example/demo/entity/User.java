f3c0e4c91836d440920ece3376bff5cbc1adab42
bala-amypo <bala@amypo.in>
2025-12-18 09:23:43 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -3,63 +3,57 @@ package com.example.demo.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import java.security.Timestamp;
 import jakarta.persistence.Id;
 
 @Entity
 public class User {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY) 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String fullName;
+
+    private String name;
     private String email;
-    private String password;
     private String role;
-    private Timestamp createdAt;
+
+    public User() {}
+
+    public User(Long id, String name, String email, String role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
-    public String getFullName() {
-        return fullName;
+
+    public String getName() {
+        return name;
     }
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+  
+    public void setName(String name) {
+        this.name = name;
     }
+  
     public String getEmail() {
         return email;
     }
+  
     public void setEmail(String email) {
         this.email = email;
     }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  
     public String getRole() {
         return role;
     }
+  
     public void setRole(String role) {
         this.role = role;
-    }
-    public Timestamp getCreatedAt() {
-        return createdAt;
-    }
-    public void setCreatedAt(Timestamp createdAt) {
-        this.createdAt = createdAt;
-    }
-    public User() {
-    }
-    public User(Long id, String fullName, String email, String password, String role, Timestamp createdAt) {
-        this.id = id;
-        this.fullName = fullName;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-        this.createdAt = createdAt;
     }
 }
