00d6025b3c939bec9f575f8e9eabdf3160fa63f6
bala-amypo <bala@amypo.in>
2025-12-18 04:57:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,9 @@
+package com.example.demo.entity;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import java.sql.Timestamp;
+@Entity
+public class User{
+    @Id
+    
+}
