d0e7f1770c26012b8777fb0c2d51c6202533474d
bala-amypo <bala@amypo.in>
2025-12-18 08:58:47 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserService userservice;
+
+    @PostMapping("/adduser")
+    public User addUser(@RequestBody User user) {
+        return userservice.createUser(user);
+    }
+
+    @GetMapping("/showusers")
+    public List<User> showUsers() {
+        return userservice.getAllUsers();
+    }
+}
