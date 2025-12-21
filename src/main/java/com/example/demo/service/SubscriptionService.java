997bebc5971fda2c5614eb299aabd6f40bb85467
bala-amypo <bala@amypo.in>
2025-12-21 08:29:57 +0000

    User commit

--- src/main/java/com/example/demo/service/SubscriptionService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import java.util.List;
-import com.example.demo.entity.SubscriptionEntity;
-
-public interface SubscriptionService {
-    SubscriptionEntity createSubscription(SubscriptionEntity sub);
-    List<SubscriptionEntity> getAllSubscriptions();
-}
